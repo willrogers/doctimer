50981a012483fa5d88ec3f0cb0d3360dfd37f906
Will Rogers <wahrogers@gmail.com>
2020-06-27 21:11:05 +0100

    Updates

---
+++ component---src-pages-index-js-bb9b23562c27ed2afaec.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{RXBc:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),l=n("Wbzz"),o=n("Bl7J"),c=n("vrFN");t.default=function(){return r.a.createElement(o.a,null,r.a.createElement(c.a,{title:"Home"}),r.a.createElement("h1",null,"Doctimer"),r.a.createElement("p",null,"Give it a try."),r.a.createElement("div",{style:{maxWidth:"300px",marginBottom:"1.45rem"}}),r.a.createElement(l.Link,{to:"/doctimer/"},"Go to the controls"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-bb9b23562c27ed2afaec.js.map
